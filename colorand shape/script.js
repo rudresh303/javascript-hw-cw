9cb143110ff37707b6e7d7e12fab58d096999190
rudresh303 <1dt18is074.rudreshgs@gmail.com>
2024-10-23 22:58:30 +0530

    commit

---
+++ colorand shape/script.js
@@ -0,0 +1,30 @@
+let round = document.querySelector(".round");
+let button = document.querySelectorAll("button");
+let shape = document.querySelector("#square");
+
+//to change the Color
+button[0].addEventListener("click", changeColor);
+
+function genarateColor(){
+    let color = Math.floor(Math.random() * 255);
+    return color;
+}
+
+function changeColor(){
+    let newColor = `rgb(${genarateColor()}, ${genarateColor()}, ${genarateColor()})`;
+    round.style.backgroundColor = newColor;
+}
+
+
+button[1].addEventListener("click", changeShape);
+let arr=["square", "round", "diamond", "triangle", "arrow", "frame", "star","cross", "left-point", "right-point", "parallal", "cheg"];
+
+function randomShape(){
+    let idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+}
+
+function changeShape(){
+    let idname = randomShape();
+    shape.id = idname;
+}
